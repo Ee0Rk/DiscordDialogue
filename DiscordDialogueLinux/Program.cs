246fd6e3677c396df689735b6aba29d00525f65f
ainawastaken <erikottofrederik@gmail.com>
2023-09-14 01:35:02 +0200

    lots of shit

---
+++ DiscordDialogueLinux/Program.cs
@@ -0,0 +1,12 @@
+﻿using Discord;
+
+namespace DiscordDialogueLinux
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
